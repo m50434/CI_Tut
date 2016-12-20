400885acb0b5a3471e7426313e52cca367fe358b
m50434 <m50434>
2016-12-20 18:37:27 +0100

    Aufgabe Lösung

--- application/controllers/Seiten.php
+++ application/controllers/Seiten.php
@@ -6,13 +6,15 @@ class Seiten extends CI_Controller {
 
 	public function view($seite = 'home'){
 		
-		if( !file_exists(APPPATH.'views/Seiten/'.$seite.'php')){
+		if( !file_exists(APPPATH.'views/Seiten/'.$seite.'.php')){
 			
 			// Diese Seite gibts nicht
 			show_404();
 		}
 		
+		
 		$data['title'] = ucfirst($seite);
+		$data['email'] = 'test@test.com';
 		$this->load->view('Seiten/'.$seite, $data);
 	}
 }
---
+++ application/views/Seiten/Impressum.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Impressum</title>
+</head>
+
+<body>
+
+<h1><?php echo $title; ?></h1>
+
+Hier steht der Inhalt vom Impressum.
+
+Meine Emailadresse: <?= $email; ?>
+</body>
+
+
+</html>
